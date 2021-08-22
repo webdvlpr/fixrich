312b4e28d8d57aed892e9d6d880579d1a7bbab70
webdeverz <85422802+webdeverz@users.noreply.github.com>
2021-08-22 20:32:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ if(innerWidth <= 425px){
     list.style.display = 'none';
 })
 }
+
+
+// Instagram-Chat-Plugin
+var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "100473028945662");
+      chatbox.setAttribute("attribution", "biz_inbox");
+
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v11.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+// Instagram-Chat-Plugin
